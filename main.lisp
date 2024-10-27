5d2f39a977275e4581b087d475ca839e29c4d100
Trevor Arjeski <tmarjeski@gmail.com>
2024-10-27 23:40:50 +0300

    keep rat in bounds

--- main.lisp
+++ main.lisp
@@ -2,18 +2,23 @@
 
 (setf +app-system+ "rat-game")
 
+(defvar *world*)
+
 (define-pool ratgame)
 
 (define-action-set in-game)
 (define-action move (directional-action in-game))
-(define-action quit-game (in-game)) 
+(define-action quit-game (in-game))
 
 (defclass main (trial:main)
-  ())
+  ()
+  (:default-initargs :context '(:width 640 :height 480
+                                :title "Rat Game")))
 
 (defmethod setup-scene ((main main) scene)
-  (enter (make-instance 'rat :name :rat :location (vec 0 0 0)) scene)
-  (enter (make-instance 'sidescroll-camera :zoom 4.0 :name :camera) scene)
+  (setf *world* scene)
+  (enter (make-instance 'rat :name :rat :location (vec 0 -240 0)) scene)
+  (enter (make-instance 'sidescroll-camera :name :camera) scene)
   (enter (make-instance 'render-pass) scene))
 
 (define-handler (scene quit-game) ()
